f7471d82f5ff61ce7301562d646620b221fc67af
djamn <david@jamnig.net>
2024-03-14 21:24:56 +0100

    added sample unit test

--- app/src/test/java/at/aau/serg/websocketdemoapp/ExampleUnitTest.java
+++ app/src/test/java/at/aau/serg/websocketdemoapp/ExampleUnitTest.java
@@ -1,17 +1,29 @@
 package at.aau.serg.websocketdemoapp;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import at.aau.serg.websocketdemoapp.networking.WebSocketClient;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    void testConcatenateStringsMethod() {
+        String first = "Hello";
+        String second = "World";
+
+        String result = WebSocketClient.concatenateStrings(first, second);
+
+        assertEquals("Hello World", result);
+    }
+
+    @Test
+    void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 }
